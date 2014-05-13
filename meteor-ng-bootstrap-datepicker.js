b9b7aaaec20c80b87eca8c09675b9431ffb9d7b7
Peter Pavlovich <pavlovich@gmail.com>
2014-05-12 22:23:20 -0400

    fixed timePicker and datesingle options

--- meteor-ng-bootstrap-datepicker.js
+++ meteor-ng-bootstrap-datepicker.js
@@ -1,11 +1,3 @@
 /**
  * Created by pavlovich on 5/5/14.
  */
-
-
-//ngMeteor.config(function($provide) {
-//    $provide.decorator('dateDirective', ['$delegate', function ($delegate) {
-//        $delegate.shift();
-//        return $delegate;
-//    }]);
-//});
--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('ngMeteor', ['client']);
-    api.use('moment-range', ['client']);
+    api.use('moment', ['client']);
 
     // Files to load in Client only.
     api.add_files([
